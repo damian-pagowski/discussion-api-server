e76dcfd4c0ccbd9bb548cdb52a0c47f7a39621fc
Damian <d.pagowski@gmail.com>
2019-12-19 20:07:16 +0700

    added endpoint for checking service status

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ const router = express.Router();
 const Post = require("../models/post");
 const Category = require("../models/category");
 
-router.get("/:category/posts", (req, res) => {
-  const category = req.params.category;
-  Category.findOne({ name: category })
-    .then(cat =>
-      Post.find({ category: cat._id })
-        .populate("category", "name -_id")
-        .then(result => res.json(result))
-    )
-    .catch(error => next(error));
-});
+router
+  .get("/:category/posts", (req, res) => {
+    const category = req.params.category;
+    Category.findOne({ name: category })
+      .then(cat =>
+        Post.find({ category: cat._id })
+          .populate("category", "name -_id")
+          .then(result => res.json(result))
+      )
+      .catch(error => next(error));
+  })
+  .get("/", (req, res) => {
+    res.json({ message: "it works" });
+  });
 
 module.exports = router;
